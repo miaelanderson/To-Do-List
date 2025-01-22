97f46a6cfdaa756aa60665c09a0c3517624f1d96
Mia <miaelanderson@gmail.com>
2025-01-22 09:30:24 -0600

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Mia Anderson on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
